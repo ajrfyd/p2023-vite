f04ac46fb775f9676dc833733c9d7bcb9dc9b190
ajrfyd <ajrfyd@naver.com>
2023-08-11 23:44:39 +0900

    7. port section

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,1 +1,28 @@
-export const port = () => console.log("port");
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+export const port = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const horSection = gsap.utils.toArray(".port__item");
+  console.log(horSection)
+
+  gsap.to(horSection, {
+    xPercent: -120 * (horSection.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#port",
+      start: "top 56px",
+      end: "+=3000",
+      pin: true,
+      scrub: 1,
+      markers: true,
+      invalidateOnRefresh: true,
+      anticipatePin: 1,
+    },
+});
+
+};
+
+
+console.log(gsap);

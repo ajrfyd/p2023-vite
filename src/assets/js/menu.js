773be0a902b61b639fba442525c5f7ffd73a34ce
ajrfyd <ajrfyd@naver.com>
2023-08-11 22:21:07 +0900

    3.header seiction

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,1 +1,13 @@
 export const menu = () => console.log("menu");
+
+const headerToggle = document.getElementById("headerToggle");
+const headerNav = document.querySelector(".header__nav");
+
+if (headerToggle) {
+  headerToggle.addEventListener("click", () => {
+  headerNav.classList.toggle("show");
+  headerToggle.getAttribute("aria-expanded") === "true"
+    ? headerToggle.setAttribute("aria-expanded", "false")
+    : headerToggle.setAttribute("aria-expanded", "true");
+  });
+}

380e3b2cdcb12cd8597363ee55841ac36de15274
ajrfyd <ajrfyd@naver.com>
2023-08-16 23:11:49 +0900

    Update: hamburger btn

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,1 +1,15 @@
-export const link = () => console.log("link");
+export const link = () => {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    console.log(anchor)
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+};
